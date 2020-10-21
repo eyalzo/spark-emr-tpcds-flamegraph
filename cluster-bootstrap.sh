9cac4631c45230a661ac19a672076a6fedef0ef4
Eyal Zohar <eyalzo@gmail.com>
2020-10-21 11:05:32 +0300

    Create cluster-bootstrap.sh

---
+++ cluster-bootstrap.sh
@@ -0,0 +1,31 @@
+### Install Uber JVM Profiler on EMR machines (AMI 2)
+
+# Install git and maven
+sudo yum -y install git-core
+sudo yum -y install maven
+
+# Clone the profiler and build it
+git clone https://github.com/uber-common/jvm-profiler.git
+cd jvm-profiler/
+# Add support to influxdb, as an optional reporter with Grafana (CPU, memory, IO)
+mvn -P influxdb clean package
+
+# Copy to the profiler folder and set permissions
+sudo mkdir /opt/profiler
+sudo cp target/jvm-profiler-1.0.0.jar /opt/profiler/.
+sudo chown hadoop:hadoop /opt/profiler/ -R
+chmod +x /opt/profiler/jvm-profiler-1.0.0.jar
+
+### Prepare influxdb configuration
+
+# Change this to your influxdb host address
+INFLUX_HOST=18.196.147.58
+
+# Prepare the config file with default influxdb settings
+PROFILER_CONFIG="/opt/profiler/influxdb.yaml"
+echo "influxdb:" > $PROFILER_CONFIG
+echo "  host: ${INFLUX_HOST}" >> $PROFILER_CONFIG
+echo "  port: 8086" >> $PROFILER_CONFIG
+echo "  database: metrics" >> $PROFILER_CONFIG
+echo "  username: admin" >> $PROFILER_CONFIG
+echo "  password: admin" >> $PROFILER_CONFIG
